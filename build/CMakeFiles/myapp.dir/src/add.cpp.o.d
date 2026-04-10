d260d3d348e16d3a868a9553b2db7167258f2bff
ldk <liudekai183@163.com>
2026-04-10 18:20:49 +0800

    删除build

--- build/CMakeFiles/myapp.dir/src/add.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/myapp.dir/src/add.cpp.o: /home/ldk/ldk/CPP_study/src/add.cpp \
- /usr/include/stdc-predef.h /home/ldk/ldk/CPP_study/include/add.h
--- build/CMakeFiles/myapp.dir/src/sub.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/myapp.dir/src/sub.cpp.o: /home/ldk/ldk/CPP_study/src/sub.cpp \
- /usr/include/stdc-predef.h /home/ldk/ldk/CPP_study/include/sub.h
