18e08d85fe4c12e2b1765ac4c446428b5088bdaa
ldk <liudekai183@163.com>
2026-04-10 18:18:57 +0800

    创建工程,cmake

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# C-_study
+# CPP_study
+
 学习c++
+
+### 编译命令：
+
+```bash
+cd build
+cmake ..
+make
+./myapp
+```
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "add.h"
+#include "sub.h"
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+    
+    cout << "a + b = " << add(a, b) << endl;
+    cout << "a - b = " << sub(a, b) << endl;
+    
+    return 0;
+}
